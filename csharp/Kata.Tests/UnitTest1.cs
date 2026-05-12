dea236065433a962ab5e2dd1e214a0e0b50b260d
Chris Wong <chris.wong.ksl@gmail.com>
2026-05-12 11:37:33 +0800

    TODO:TwoRectanglesV2

--- csharp/Kata.Tests/UnitTest1.cs
+++ csharp/Kata.Tests/UnitTest1.cs
@@ -18,4 +18,22 @@ public class Tests
     {
         Assert.That(Kata.Calculate(new[] { new[] { 0, 0, 1, 1 } }), Is.EqualTo(1));
     }
+
+    [Test, Order(3)]
+    public void OneRectangleV2()
+    {
+        Assert.That(Kata.Calculate(new[] { new[] { 0, 4, 11, 6 } }), Is.EqualTo(22));
+    }
+
+    [Test, Order(4)]
+    public void TwoRectangles()
+    {
+        Assert.That(Kata.Calculate(new[] { new[] { 0, 0, 1, 1 }, new[] { 1, 1, 2, 2 } }), Is.EqualTo(2));
+    }
+
+    [Test, Order(5)]
+    public void TwoRectanglesV2()
+    {
+        Assert.That(Kata.Calculate(new[] { new[] { 0, 0, 1, 1 }, new[] { 0, 0, 2, 2 } }), Is.EqualTo(4));
+    }
 }
