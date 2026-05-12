4e33b1b424dd95f8a5232b5bed5b4f9df06584fc
Chris Wong <chris.wong.ksl@gmail.com>
2026-05-12 11:36:22 +0800

    OneRectangle

--- csharp/Kata.Tests/UnitTest1.cs
+++ csharp/Kata.Tests/UnitTest1.cs
@@ -12,4 +12,10 @@ public class Tests
     {
         Assert.That(Kata.Calculate(Enumerable.Empty<int[]>()), Is.EqualTo(0));
     }
+
+    [Test, Order(2)]
+    public void OneRectangle()
+    {
+        Assert.That(Kata.Calculate(new[] { new[] { 0, 0, 1, 1 } }), Is.EqualTo(1));
+    }
 }
--- csharp/Kata/Kata.cs
+++ csharp/Kata/Kata.cs
@@ -6,6 +6,11 @@ public class Kata
   {
     // your code here...
     if (rectangles.Count() == 0) return 0;
-    return -1;
+    long area = 0;
+    foreach (var rectangle in rectangles)
+    {
+      area += (long)(rectangle[2] - rectangle[0]) * (long)(rectangle[3] - rectangle[1]);
+    }
+    return area;
   }
 }
